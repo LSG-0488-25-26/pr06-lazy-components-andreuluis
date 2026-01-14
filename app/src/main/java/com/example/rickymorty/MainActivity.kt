dae357b6ceddc01fd792fb1e9570373f46e341eb
lluisp7 <luisperezutreras@gmail.com>
2026-01-14 15:52:00 +0100

    character itemi navhost

--- app/src/main/java/com/example/rickymorty/MainActivity.kt
+++ app/src/main/java/com/example/rickymorty/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import com.example.rickymorty.ui.theme.RickymortyTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +19,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            RickymortyTheme {
+            RickyMortyTheme {
+                val navController = rememberNavController()
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    RickAndMortyNavHost(
+                        modifier = Modifier.padding(innerPadding),
+                        navController = navController
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RickymortyTheme {
-        Greeting("Android")
     }
 }
